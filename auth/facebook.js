f2564f1107cb442852506fb08d14574bf061b7cf
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-20 14:11:40 -0400

    addressed Matt's comments

--- auth/facebook.js
+++ auth/facebook.js
@@ -9,41 +9,33 @@ if (!process.env.FACEBOOK_APP_ID || !process.env.FACEBOOK_APP_SECRET) {
 
 } else {
 
-  router.get('/', (req, res, next) => {
-    console.log('got to facebook auth')
-    res.send('got to facebook auth')
-  })
-
   router.post('/', (req, res, next) => {
-    console.log('incoming token:', req.body.token)
     axios.get(`https://graph.facebook.com/me?fields=name,id,email,picture&access_token=${req.body.token}`)
-    .then(result => result.data)
-    .then(fbUser => {
-      console.log('user from FB:', fbUser)
-      User.find({
-        where: { facebookId: fbUser.id }
-      })
-      .then(foundUser => {
-        if (foundUser === null) {
-          console.log('creating new user...')
-          User.create({
-            userName: fbUser.name,
-            email: fbUser.email,
-            facebookId: fbUser.id,
-            facebookPic: fbUser.picture.data.url,
+      .then(result => result.data)
+      .then(fbUser => {
+        User.find({
+          where: { facebookId: fbUser.id }
+        })
+          .then(foundUser => {
+            if (foundUser === null) {
+              User.create({
+                userName: fbUser.name,
+                email: fbUser.email,
+                facebookId: fbUser.id,
+                facebookPicUrl: fbUser.picture.data.url,
+              })
+                .then(newUser => {
+                  res.send(newUser)
+                })
+            } else {
+              res.send(foundUser)
+            }
           })
-          .then(newUser => {
-            res.send(newUser)
-            console.log('user sent back to client')
-          })
-          .catch(err => console.log(err))
-        } else {
-          console.log('found a user')
-          res.send(foundUser)
-          console.log('user sent back to client')
-        }
+          .catch(next)
       })
-    })
-    .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(401)
+      })
   })
 }
--- db/models/user.js
+++ db/models/user.js
@@ -31,7 +31,7 @@ const User = db.define('user', {
         unique: true,
 
     },
-    facebookPic: {
+    facebookPicUrl: {
         type: Sequelize.STRING
     },
     uuid: {
