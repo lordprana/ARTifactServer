630f12db9c0b2ac44a27c6856bd426e0dd699473
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-27 16:15:52 -0400

    now using async file operations with async/await

--- api/style-image.js
+++ api/style-image.js
@@ -3,26 +3,24 @@ const fs = require('fs');
 const { deepStyle } = require('../deep-style/connector');
 module.exports = router;
 
-router.post('/', (req, res, next) => {
-  const inFile = `${req.auth}-in.jpg`
-  const outFile = `${req.auth}-out.jpg`
-  const path = './deep-style/tmp/'
-  const photo = Buffer.from(req.body.photo, 'base64')
+router.post('/', async (req, res, next) => {
+  try {
+    const inFile = `${req.auth}-in.jpg`
+    const outFile = `${req.auth}-out.jpg`
+    const path = './deep-style/tmp/'
+    const photo = Buffer.from(req.body.photo, 'base64')
 
-  fs.writeFileSync(path + inFile, photo, err => console.log(err))
-  deepStyle(req.body.style, inFile, outFile)
-  .then(_ => {
-    const styledImageBuffer = fs.readFileSync(path + outFile)
+    await fs.writeFile(path + inFile, photo, err => console.log(err))
+    await deepStyle(req.body.style, inFile, outFile)
+
+    const styledImageBuffer = await fs.readFile(path + outFile)
     const styledImageBase64 = styledImageBuffer.toString('base64')
     res.send(styledImageBase64)
     //fs.unlink(path + inFile)
     //fs.unlink(path + outFile)
-  })
-  .catch(err => {
-    console.error(err)
-    res.sendStatus(500)
-  })
+  } catch (err) {
+    next(err)
+  }
 });
 
 // uri should be prepended with data:image/jpeg;base64,
-//make fs async
